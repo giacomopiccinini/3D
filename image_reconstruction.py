64744ee6ffa82f63727122ec03045cddcdd327ab
Giacomo Piccinini <piccinini.giacomo@gmail.com>
2022-09-09 14:55:18 +0200

    [FIX] Fixed and streamlined reconstruction

--- image_reconstruction.py
+++ image_reconstruction.py
@@ -5,16 +5,10 @@ def points_to_image(points, shape):
 
     """ Reconstruct depth map given points coming from 3D analysis"""
 
-    # Initialise image to be filled 
-    image  = np.zeros(shape)
+    # Extract depth
+    z = points[:, 2]
 
-    # Extract y, x, z values from points
-    y, x, z = points
-
-    # Construct the grid in Numpy
-    x, y = np.meshgrid(x, y)
-
-    # Fill image with depth
-    image[y, x] = z
+    # Recreate image
+    image = np.full(shape, z.reshape(shape))
 
     return image
